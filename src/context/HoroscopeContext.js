8dae1c4f3d55c83472f7685755bb12bc7e74d816
Charlie8rown <105841366+Charlie8rown@users.noreply.github.com>
2023-03-02 09:47:03 -0800

    Part finished

--- src/context/HoroscopeContext.js
+++ src/context/HoroscopeContext.js
@@ -1,3 +1,16 @@
-import { createContext } from "react";
+import { useEffect, useState, createContext } from "react";
+import App from "../App";
+import HoroscopesData from "../data/horoscopes";
 
 export const HoroscopeContext = createContext();
+
+export default function HoroscopeProvider(props) {
+  const [getter, setSign] = useState("Scorpio");
+  const signInfo = HoroscopesData[getter];
+
+  return (
+    <HoroscopeContext.Provider value={{ signInfo, setSign }}>
+      {props.children}
+    </HoroscopeContext.Provider>
+  );
+}
